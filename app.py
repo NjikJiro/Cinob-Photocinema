b61eb40ea044df00b6f4616e7e94bf5afde79f1a
RaihanMaulanaa <raihanmaulanasyahputraa@gmail.com>
2023-09-16 09:21:00 +0700

    progress create data
    - berhasil menambahkan data
    - berhasil membuat folder saat menambahkan tiap data di folder
    img > folder_new

--- app.py
+++ app.py
@@ -82,16 +82,45 @@ def dashboard():
 @app.route('/posting', methods=['POST'])
 def posting():
     token_receive = request.cookies.get(TOKEN_KEY)
-    # try:
-    #     payload = jwt.decode(
-    #         token_receive,
-    #         SECRET_KEY,
-    #         algorithms=['HS256']
-    #     )
-    #     user_info = db.users.find_one({'username': payload.get('id')})
-    #     # buat kode input data disini
-    # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-    #     return redirect(url_for('home'))
+    try:
+        payload = jwt.decode(
+            token_receive,
+            SECRET_KEY,
+            algorithms=['HS256']
+        )
+        user_info = db.users.find_one({'username': payload.get('id')})
+        # buat kode input data disini
+        title_receive = request.form.get('title_give')
+        file = request.files['file_give']
+
+        # Mencari nomor folder terakhir
+        last_folder_number = list(
+            db.product.find().sort([('folder', -1)]).limit(1))
+        if len(last_folder_number) == 0 or 'folder' not in last_folder_number[0]:
+            detail = "detail 1"
+        else:
+            last_number = int(
+                last_folder_number[0]['folder'].replace('detail', ''))
+            detail = f"detail {last_number + 1}"
+
+        directory = f'static/img/{detail}'
+        os.makedirs(directory, exist_ok=True)
+        # akhir kode cari folder
+
+        extension = file.filename.split('.')[1]
+        filename = f'{directory}/{title_receive}.{extension}'
+        file.save(filename)
+
+        doc = {
+            'username': user_info.get('username'),
+            'title': title_receive,
+            'file': filename,
+            'folder': detail,
+        }
+        db.product.insert_one(doc)
+        return jsonify({'msg': 'data telah ditambahkan'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for('dashboard'))
 
 
 @app.route('/login_save', methods=['POST'])
--- static/js/main.js
+++ static/js/main.js
@@ -29,3 +29,26 @@ function sign_out() {
   alert("Anda telah keluar");
   window.location.href = "/login";
 }
+
+function posting() {
+  let title = $("#input-title").val().trim();
+  let file = $("#input-file").prop("files")[0];
+
+  // membuat objek formData
+  form_data = new FormData();
+
+  form_data.append("title_give", title);
+  form_data.append("file_give", file);
+
+  $.ajax({
+    type: "POST",
+    url: "/posting",
+    data: form_data,
+    contentType: false,
+    processData: false,
+    success: function (response) {
+      alert(response["msg"]);
+      window.location.reload();
+    },
+  });
+}
